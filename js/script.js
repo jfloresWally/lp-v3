6059e8d7fb2ffe98a5ade828d03ed50a4492cf74
jfloresWally <106196681+jfloresWally@users.noreply.github.com>
2023-04-25 15:19:42 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// Preguntas frecuentes
+const items = document.querySelectorAll('.accordion button');
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute('aria-expanded');
+
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute('aria-expanded', 'false');
+  }
+
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+
+items.forEach((item) => item.addEventListener('click', toggleAccordion));
+
+// Menu mobile
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
